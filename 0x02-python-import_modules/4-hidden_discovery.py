02f66bea1009a67552ebbff98e9eca5d03c2b25a
MainaGitHUB <mainatrevor45@gmail.com>
2022-06-13 01:07:01 -0700

    New commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
 
+i = 0
 
-def principal():
-    for i in dir(hidden_4):
-        if not (i[0] == '_' and i[1] == '_'):
-            print(i)
-
-if __name__ == "__main__":
-    principal()
+if __name__ == '__main__':
+    mylist = dir(hidden_4)
+    newlist = sorted(mylist)
+    while i < len(newlist):
+        if newlist[i][0] != '_':
+            print(newlist[i])
+        i += 1
